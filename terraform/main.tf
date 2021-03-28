1ecdc228c22722eaeeef72ec6bd44c26211d6763
Thomas Weibel <thom@ikhono.net>
2021-03-28 23:22:11 +0200

    Create public IP

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,3 +39,12 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/24"]
 }
+
+# Create public IP
+resource "azurerm_public_ip" "publicip" {
+  name                = "pip-udacity"
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
