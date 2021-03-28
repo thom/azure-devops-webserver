17ab5468d6b03d9be7673d024abe1f24b17b5bd1
Thomas Weibel <thom@ikhono.net>
2021-03-29 00:37:15 +0200

    Create sample virtual machine

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,3 +81,46 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.publicip.id
   }
 }
+
+variable "admin_username" {
+    type = string
+    description = "Administrator user name for virtual machine"
+}
+
+variable "admin_password" {
+    type = string
+    description = "Password must meet Azure complexity requirements"
+}
+
+# Create a Linux virtual machine
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "vm-udacity"
+  location              = "eastus"
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_B1s"
+
+  storage_os_disk {
+    name              = "osdisk-udacity"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04.0-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "vm-udacity"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
