a19580c0b1e06ca2bfd66833a100f4e9ee35b6e0
Thomas Weibel <thom@ikhono.net>
2021-03-29 00:38:42 +0200

    Add data source for public IP

--- terraform/main.tf
+++ terraform/main.tf
@@ -124,3 +124,10 @@ resource "azurerm_virtual_machine" "vm" {
     disable_password_authentication = false
   }
 }
+
+# Create data source for public IP
+data "azurerm_public_ip" "ip" {
+  name                = azurerm_public_ip.publicip.name
+  resource_group_name = azurerm_virtual_machine.vm.resource_group_name
+  depends_on          = [azurerm_virtual_machine.vm]
+}
