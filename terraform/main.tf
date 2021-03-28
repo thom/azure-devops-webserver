3703e3492076d57711708528124106b1ebd14b4c
Thomas Weibel <thom@ikhono.net>
2021-03-28 23:07:25 +0200

    Create networks
    Create virtual network and a subnet on that virtual network

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = ">= 2.26"
     }
   }
@@ -12,13 +12,30 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "main" {
+# Create resource group
+resource "azurerm_resource_group" "rg" {
   name     = "rg-udacity"
   location = "eastus"
   tags = {
-      dept = "Engineering"
-      environment = "Production"
-      project = "Udacity Cloud DevOps using Microsoft Azure Nanodegree Program: Deploying a Web Server in Azure"
-      createdby = "Terraform"
+    dept        = "Engineering"
+    environment = "Production"
+    project     = "Udacity Cloud DevOps using Microsoft Azure Nanodegree Program: Deploying a Web Server in Azure"
+    createdby   = "Terraform"
   }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-udacity"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "snet-udacity"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
 }
