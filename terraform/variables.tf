6e686a88b67e1162e55959d38536bd66aab2ab53
Thomas Weibel <thom@ikhono.net>
2021-03-29 21:53:38 +0200

    Add new variables
    Add project and location variables, and move admin_username and
    admin_password to variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,32 @@
-variable "tags" {
-  type = map
+variable "location" {
+  type        = string
+  description = "Region resources are created in"
+  default     = "eastus"
+}
 
+variable "tags" {
+  type        = map(string)
+  description = "Tags for resources"
   default = {
     dept        = "Engineering"
     environment = "Production"
     project     = "Udacity Cloud DevOps using Microsoft Azure Nanodegree Program: Deploying a Web Server in Azure"
     createdby   = "Terraform"
   }
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Administrator user name for virtual machine"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Password must meet Azure complexity requirements"
+}
+
+variable "project" {
+  type    = string
+  description = "Project name appended to resource names"
+  default = "udacity-web-server"
 }
