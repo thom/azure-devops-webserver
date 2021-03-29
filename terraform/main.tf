572f2c90a7ff3b5d68c000acfb2ebee71002e9b5
Thomas Weibel <thom@ikhono.net>
2021-03-29 21:32:13 +0200

    Fix VM SKU
    Change VM SKU to 18.04-LTS

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,7 +110,7 @@ resource "azurerm_virtual_machine" "vm" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04.0-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
