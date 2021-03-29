5bed1f563ddff52a2cc569357769c13d10b21827
Thomas Weibel <thom@ikhono.net>
2021-03-29 22:10:19 +0200

    Use project name in resource names

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,14 +14,14 @@ provider "azurerm" {
 
 # Create resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-udacity"
+  name     = "rg-${var.project}"
   location = var.location
   tags     = var.tags
 }
 
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-udacity"
+  name                = "vnet-${var.project}"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -30,7 +30,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 # Create subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "snet-udacity"
+  name                 = "snet-${var.project}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/24"]
@@ -38,7 +38,7 @@ resource "azurerm_subnet" "subnet" {
 
 # Create public IP
 resource "azurerm_public_ip" "publicip" {
-  name                = "pip-udacity"
+  name                = "pip-${var.project}"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
@@ -48,7 +48,7 @@ resource "azurerm_public_ip" "publicip" {
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "nsg" {
-  name                = "nsg-udacity"
+  name                = "nsg-${var.project}"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   tags                = var.tags
@@ -68,13 +68,13 @@ resource "azurerm_network_security_group" "nsg" {
 
 # Create network interface
 resource "azurerm_network_interface" "nic" {
-  name                      = "nic-udacity"
+  name                      = "nic-${var.project}"
   location                  = var.location
   resource_group_name       = azurerm_resource_group.rg.name
   tags                      = var.tags
 
   ip_configuration {
-    name                          = "nic-config-udacity"
+    name                          = "nic-config-${var.project}"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.publicip.id
@@ -83,7 +83,7 @@ resource "azurerm_network_interface" "nic" {
 
 # Create a Linux virtual machine
 resource "azurerm_virtual_machine" "vm" {
-  name                  = "vm-udacity"
+  name                  = "vm-${var.project}"
   location              = var.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
@@ -91,7 +91,7 @@ resource "azurerm_virtual_machine" "vm" {
   tags                  = var.tags
 
   storage_os_disk {
-    name              = "osdisk-udacity"
+    name              = "osdisk-${var.project}"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
@@ -105,7 +105,7 @@ resource "azurerm_virtual_machine" "vm" {
   }
 
   os_profile {
-    computer_name  = "vm-udacity"
+    computer_name  = "vm-${var.project}"
     admin_username = var.admin_username
     admin_password = var.admin_password
   }
